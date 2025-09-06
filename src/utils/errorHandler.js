1970ffbc42d5328dd866cc833e11f69448670043
Nitin Pinnamaneni <nitinpinnamaneni@yahoo.com>
2025-09-05 20:15:48 -0400

    package.json and .env

---
+++ src/utils/errorHandler.js
@@ -0,0 +1,7 @@
+const logger = require('./logger');
+const errorHandler = (err, req, res, next) => {
+    logger.error(err);
+    res.status(500).json({ error: 'Something went wrong.' });
+};
+
+module.exports = errorHandler;
